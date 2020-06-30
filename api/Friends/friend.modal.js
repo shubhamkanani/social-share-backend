ad6df77e6078dbbc8713aa4a184a7013763cb139
Shubham_Kanani <shubhamkanani605@gmail.com>
2020-06-30 18:11:03 +0530

    friend updata

---
+++ api/Friends/friend.modal.js
@@ -0,0 +1,11 @@
+import mongoose, {Schema} from 'mongoose';
+import timestamps from "mongoose-timestamp";
+
+const friendSchema = Schema({
+    userId:String,
+    friendList:Array,
+    getRequest:Array,
+    sentRequest:Array
+}, { timestamps: {createdAt:'created_at'} })
+
+export const FriendList = mongoose.model('userInfo',friendSchema);
---
+++ api/Friends/index.js
@@ -0,0 +1,2 @@
+export * from './friend.modal';
+export * from './friend.restRoute'
