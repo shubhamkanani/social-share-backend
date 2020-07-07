6c4fa96bcfa6ecc227f217486aefbe4643198a96
Shubham_Kanani <shubhamkanani605@gmail.com>
2020-07-07 12:44:43 +0530

    update in profile and cover image

--- api/users/user.restRouter.js
+++ api/users/user.restRouter.js
@@ -12,6 +12,7 @@ import fs from 'fs';
 import configKey from '../../config'
 import jwt from 'jsonwebtoken';
 import { Users } from "./user.modal";
+import { decode } from "querystring";
 export const userRouter = express.Router();
 
 var pstorage = multer.diskStorage({
@@ -52,8 +53,55 @@ var cstroge = multer.diskStorage({
         cb(null, data._id+'.'+extension)
     }
 })
-const uploadCoverImg = multer({storage:cstroge})
-const uploadProfileImg = multer({storage:pstorage})
+const uploadCoverImg = multer({storage:cstroge,
+    fileFilter:async function (req, file, cb){
+        try{
+            const decoded = await jwt.verify(req.headers.token, configKey.secrets.JWT_SECRET);
+              
+        const data = await Users.findOne({emailId:decoded.sub})
+            const extension = data.coverImgURl.substring(data.coverImgURl.lastIndexOf(".") + 1);
+            const coverUploaddir = path.join(__dirname,'..','..','public','cover');
+            console.log(extension);
+            
+            if(fs.existsSync(coverUploaddir+'/'+data._id+'.'+extension)){
+                fs.unlink(coverUploaddir+'/'+data._id+'.'+extension, function (err) {
+                    if (err) throw err;
+                    // if no error, file has been deleted successfully
+                    console.log('File deleted!');
+                });
+            }
+            cb(null,true);
+        }
+        catch(err){
+            cb(null,false,new Error(err.message));
+        }
+
+}
+})
+const uploadProfileImg = multer({storage:pstorage,
+    fileFilter:async function (req, file, cb){
+        try{
+            const decoded = await jwt.verify(req.headers.token, configKey.secrets.JWT_SECRET);
+        const data = await Users.findOne({emailId:decoded.sub})
+            const extension = data.coverImgURl.substring(data.coverImgURl.lastIndexOf(".") + 1);
+            const profileUploadDir = path.join(__dirname,'..','..','public','profile');
+            console.log(extension);
+            
+            if(fs.existsSync(profileUploadDir+'/'+data._id+'.'+extension)){
+                fs.unlink(profileUploadDir+'/'+data._id+'.'+extension, function (err) {
+                    if (err) throw err;
+                    // if no error, file has been deleted successfully
+                    console.log('File deleted!');
+                });
+            }
+            cb(null,true);
+        }
+        catch(err){
+            cb(null,false,new Error(err.message));
+        }
+
+}
+})
 
 userRouter.post('/setprofileimg', uploadProfileImg.single('image'),setProfileImg);
 userRouter.post('/setcoverimg',uploadCoverImg.single('image'),setCoverImg);
